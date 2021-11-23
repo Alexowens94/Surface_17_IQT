a990dd33a7305f5c90a74fa2250244b947c56a67
daisyrainsmith <ds572@sussex.ac.uk>
2021-11-23 14:54:03 +0000

    PDD and dressed state error models

---
+++ compiled_surface_code_pdd_error_model.py
@@ -0,0 +1,150 @@
+import random
+import numpy as np
+import json
+from math import sqrt, pi
+from projectq.ops import All, Measure, X, Y, Z, Rx, Ry, Rxx
+
+def insert_2q_ctrl_error(qubit1, qubit2):
+    X | qubit1
+    X | qubit2
+
+def insert_2q_motional_error(qubit1, qubit2):
+    X | qubit1
+    X | qubit2
+
+def insert_1q_x_ctrl_error(qubit):
+    X | qubit
+
+def insert_1q_y_ctrl_error(qubit):
+    Y | qubit
+
+def insert_1q_dephasing_error(qubit):
+    Z | qubit
+
+def x_type_entangling(dataq,ancillaq,s=1,pxctrl=0,pxxctrl=0,pmot=0,pd=0):
+    Rxx(s*pi/4) | (dataq,ancillaq)
+    if random.random()<pxxctrl:
+        insert_2q_ctrl_error(dataq, ancillaq)
+    if random.random()<pmot:
+        insert_2q_motional_error(dataq, ancillaq)
+    if random.random()<pd:
+        insert_1q_dephasing_error(dataq)
+    if random.random()<pd:
+        insert_1q_dephasing_error(ancillaq)
+    Rx(-s*pi/2) | dataq
+    if random.random()<pxctrl:
+        insert_1q_x_ctrl_error(dataq)
+    if random.random()<pd:
+        insert_1q_dephasing_error(dataq)
+
+def z_type_entangling(dataq,ancillaq,v=1,s=1,pxctrl=0,pxxctrl=0,pyctrl=0,pmot=0,pd=0):
+    Ry(v*pi/2) | dataq
+    if random.random()<pyctrl:
+        insert_1q_y_ctrl_error(dataq)
+    if random.random()<pd:
+        insert_1q_dephasing_error(dataq)
+    Rxx(s * pi / 4) | (dataq, ancillaq)
+    if random.random() < pxxctrl:
+        insert_2q_ctrl_error(dataq, ancillaq)
+    if random.random() < pmot:
+        insert_2q_motional_error(dataq, ancillaq)
+    if random.random() < pd:
+        insert_1q_dephasing_error(dataq)
+    if random.random() < pd:
+        insert_1q_dephasing_error(ancillaq)
+    Rx(-s * pi / 2) | dataq
+    if random.random()<pxctrl:
+        insert_1q_x_ctrl_error(dataq)
+    if random.random()<pd:
+        insert_1q_dephasing_error(dataq)
+    Ry(v * pi / 2) | dataq
+    if random.random() < pyctrl:
+        insert_1q_y_ctrl_error(dataq)
+    if random.random() < pd:
+        insert_1q_dephasing_error(dataq)
+
+def stabiliser_timestep_1(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+        x_type_entangling(data[4], ancilla[1], pxctrl=0,pyctrl=0,pmot=0,pd=0)
+        x_type_entangling(data[8], ancilla[6], pxctrl=0,pyctrl=0,pmot=0,pd=0)
+        x_type_entangling(data[6], ancilla[4], pxctrl=0,pyctrl=0,pmot=0,pd=0)
+
+def stabiliser_timestep_2(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+        x_type_entangling(data[1], ancilla[1], pxctrl=0,pyctrl=0,pmot=0,pd=0)
+        x_type_entangling(data[3], ancilla[4], pxctrl=0,pyctrl=0,pmot=0,pd=0)
+        x_type_entangling(data[5], ancilla[6], pxctrl=0,pyctrl=0,pmot=0,pd=0)
+
+def stabiliser_timestep_3(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+    x_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    x_type_entangling(data[3], ancilla[4], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    x_type_entangling(data[5], ancilla[6], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+
+def stabiliser_timestep_4(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+    x_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    x_type_entangling(data[3], ancilla[4], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    x_type_entangling(data[5], ancilla[6], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+
+def stabiliser_timestep_5(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+
+def stabiliser_timestep_6(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+
+def stabiliser_timestep_7(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+
+def stabiliser_timestep_8(data,ancilla,v=1,s=1,pxctrl=0,pyctrl=0,pmot=0,pd=0):
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+    z_type_entangling(data[1], ancilla[1], pxctrl=0, pyctrl=0, pmot=0, pd=0)
+
+def stabilizer_cycle(data, ancilla, eng, reset=True, pxctrl=0,pxxctrl=0,pyctrl=0,pmot=0,pd=0):
+    '''
+    :param data: list of the data qubits
+    :param ancilla: list of the ancilla qubits
+    :param eng:
+    :param reset:
+    :return:
+    '''
+    if len(data)!=9:
+        raise Exception('data qubit register does not correspond to the surface 17 QEC code')
+    stabiliser_timestep_1(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+    stabiliser_timestep_2(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+    stabiliser_timestep_3(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+    stabiliser_timestep_4(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+    stabiliser_timestep_5(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+    stabiliser_timestep_6(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+    stabiliser_timestep_7(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+    stabiliser_timestep_8(data,ancilla,pxctrl,pxxctrl,pyctrl,pmot,pd)
+
+    All(Measure) | ancilla
+    eng.flush()
+    syndrome_t = [int(q) for q in ancilla]
+    if reset:
+        for a in ancilla: #reset the ancillas to 0 at end of stab round (allow for repeat rounds)
+            if int(a) == 1:
+                X | a
+    return syndrome_t
+
+def get_results_log_e(rounds, runs, incorrect_count, time):
+    res = {
+        "runs": runs,
+        "rounds": rounds,
+        "incorrect_count": incorrect_count,
+        "time_taken": time,
+          }
+    return res
+
+def get_results_log_e_run_til_fail(rounds, time, p1q, p2q):
+    res = {
+        "rounds_til_fail": rounds,
+        "p1q_error": p1q,
+        "p2q_error": p2q,
+        "time_taken": time,
+          }
+    return res
