5c572ecbf6b71e61dafdc16a8de4b64fe38ed5db
AlexOwens <alexowens94@hotmail.co.uk>
2021-12-30 17:23:36 +0000

    Assorted uncommitted changes pre file culling A.O

--- log_e_rate_run_til_fail.py
+++ log_e_rate_run_til_fail.py
@@ -6,22 +6,23 @@ from projectq import MainEngine
 from projectq.backends import Simulator
 
 eng = MainEngine(Simulator())
-ancilla_number=8
-data_number=9
-result_dict = {}
-round_count_list=[]
+ancilla_number = 8
+data_number = 9
+round_count_list = []
 
-run_name = 'test_log_e_rate_til_fail_pleak0'
+run_name = 'test_log_e_rate_til_fail_p20_ancilla_data_repumped'
 
-number_of_runs = 200
+number_of_runs = 2
 p1q = 0.001
-p2q = 0.01
-p_leak = 0
-leaked_q_reg = 17*[0]  # a register (initialised as 0s) to track which qubits  leaked, LEAKED == 1 0-8 data,9-16 ancilla
+p2q = 0
+p_leak = 0.00
+bad_hook_order = False  # set true to take stab meas in wrong order, single qubit errors mid cycle can give hook errors
+
 correction_table = load_lookup_table('correction_table_depolarising.json')
 
 t_begin = time.perf_counter()
 for _ in range(number_of_runs):
+    leaked_q_reg = 17 * [0]  # register (init as 0s) to track which qubits  leaked, LEAKED == 1 0-8 data,9-16 ancilla
     round_count = 0
     #max_rounds = 60
     #while round_count < max_rounds:
@@ -42,14 +43,14 @@ for _ in range(number_of_runs):
         prev_syndrome=np.array(quiescent)
 
         syndrome = np.array(stabilizer_cycle(data, ancilla, leaked_q_reg, eng, reset=True,
-                                             p1q=p1q, p2q=p2q, p_leak=p_leak))
+                                             bad_hook_order=bad_hook_order, p1q=p1q, p2q=p2q, p_leak=p_leak))
         flips_a = (prev_syndrome - syndrome) % 2
         prev_syndrome = syndrome
         if np.all((flips_a == 0)):
             ft_syndrome = flips_a
         else:
-            syndrome = np.array(stabilizer_cycle(data, ancilla, leaked_q_reg, eng, reset=True, old_order=True,
-                                                 p1q=p1q, p2q=p2q, p_leak=p_leak))
+            syndrome = np.array(stabilizer_cycle(data, ancilla, leaked_q_reg, eng, reset=True,
+                                                 bad_hook_order=bad_hook_order, p1q=p1q, p2q=p2q, p_leak=p_leak))
             flips_b = (prev_syndrome - syndrome) % 2
             prev_syndrome = syndrome
             ft_syndrome = (flips_a + flips_b) %2
@@ -62,11 +63,14 @@ for _ in range(number_of_runs):
         All(Measure) | data
         eng.flush()  # flush all gates (and execute measurements)
         data_meas = [int(q) for q in data]
+        # print('leaked q reg, data meas')
+        # print(leaked_q_reg)
+        # print(data_meas)
         ### measure leaked states as bright
         for i, q in enumerate(leaked_q_reg[:9]):
             if int(q) == 1:
                 data_meas[i] = 1
-                print('leak registered at data meas')
+                #print('leak registered at data meas')
         logic_Z_meas = sum(data_meas)%2
         if logic_Z_meas == 1: #incorrect
             print('incorrect logic meas {}'.format(data_meas))
@@ -75,16 +79,8 @@ for _ in range(number_of_runs):
 
     t_stop = time.perf_counter()
     time_taken = t_stop-t_start
-
-
-    # if str(round_count) in result_dict:
-    #     result_dict[str(round_count)] += 1
-    # else:
-    #     result_dict[str(round_count)] = 1
     round_count_list.append(round_count)
-    #
-    # print('{} QEC rounds til failure'.format(round_count))
-    # print('time taken {}s'.format(time_taken))
+
 t_end = time.perf_counter()
 total_time_taken = t_end - t_begin
 print('total time taken {}'.format(total_time_taken))
@@ -94,7 +90,6 @@ results={
     'p1q_error': p1q,
     'p2q_error': p2q,
     'p_leak_error': p_leak,
-    #'rounds_til_fail_tally': result_dict,
     'rounds_til_fail_list': round_count_list,
     'total_time_taken_{}_runs'.format(number_of_runs): total_time_taken
 }
