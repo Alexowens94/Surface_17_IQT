adc2e6109e169dbfd862e93ec64392628da66d23
Daisy Smith <ds572@sussex.ac.uk>
2022-01-21 16:22:03 +0000

    Restructuring logical qubit - generate all error and gate locations upon initialization of a StabilizerCycle, and generate a list of errors to insert based on an error subset and the error model.

--- logical_qubit.py
+++ logical_qubit.py
@@ -3,7 +3,13 @@ from math import pi
 
 from projectq import MainEngine
 from projectq.backends import Simulator
-from projectq.ops import All, Measure, X, Z, H, Rx, Ry, Rxx, Z, Rzz
+from projectq.ops import All, Measure, X, Z, H, Rx, Ry, Rxx, Rzz
+from error import *
+
+
+pdd_error_model = {Rxx : [XCtrlError, DephasingError], Rx : [XCtrlError, DephasingError], Ry : [YCtrlError, DephasingError]}
+
+#dressed_state_error_model = {Rxx : [XCtrlError, LeakageError], Rx : [XCtrlError, LeakageError], Ry : [YCtrlError, LeakageError]}
 
 class NoisyGate():
     """
@@ -12,17 +18,31 @@ class NoisyGate():
     for that gate. Depending on the circuit-scheme and the
     error model in use.
     """
-    def __init__(self, gate, qubits, location=None):
+    def __init__(self, gate, qubits, location=None, error_model={}):
         self.gate = gate
         self.qubits = qubits
         self.location = location
+        self.error_model = error_model
+        self.possible_errors = self.get_errors_from_error_model()
 
-    def apply(self, error_list=None):
+    def __str__(self):
+        return "NoisyGate: \n Gate: {}, \n Qubits : {}, \n Location: {}, \n Error model: {}, \n " \
+               "Errors, {}".format(self.gate, self.qubits, self.location, self.error_model, self.possible_errors)
+
+    def get_errors_from_error_model(self):
+        errors = []
+        for gate in self.error_model.keys():
+            if type(self.gate) == gate:
+                for error in self.error_model[gate]:
+                    errors.append(error())
+        return errors
+
+    def apply(self, error_list=[]):
         self.gate | (self.qubits)
         for error in error_list:
-            if self.location == error.location:
+            if error.location == self.location:
                 error.insert_error(*self.qubits)
-                print("error {} inserted at {}".format(error.name, self.location))
+                print("Error: {} inserted at {}".format(error.name, self.location))
 
 
 class EntanglingOperation():
@@ -31,10 +51,11 @@ class EntanglingOperation():
     This class is not meant to be used directly but is inherited by classes such
     as XEntanglingOp."""
 
-    def __init__(self, location=None, dataq=None, ancillaq=None, cancel_data_rx=None, s=None, v=None):
+    def __init__(self, location=None, dataq=None, ancillaq=None, cancel_data_rx=None, s=None, v=None, error_model=None):
         self.location = location
         self.dataq = dataq
         self.ancillaq = ancillaq
+        self.error_model = error_model
         self.cancel_data_rx = cancel_data_rx
         self.s=s
         self.v=v
@@ -43,7 +64,7 @@ class EntanglingOperation():
     def build_noisy_gates(self):
         raise Exception("No build_noisy_gates function defined.")
 
-    def run(self, error_list=None):
+    def run(self, error_list=[]):
         for noisy_gate in self.noisy_gates:
             noisy_gate.apply(error_list=error_list)
 
@@ -53,10 +74,10 @@ class XTypeEntanglingOp(EntanglingOperation):
 
     def build_noisy_gates(self):
         noisy_gates = []
-        rxx_gate = NoisyGate(Rxx(self.s * pi / 2), [self.dataq, self.ancillaq], self.location + [0])
+        rxx_gate = NoisyGate(Rxx(self.s * pi / 2), [self.dataq, self.ancillaq], self.location + [0], error_model=self.error_model)
         noisy_gates.append(rxx_gate)
         if not self.cancel_data_rx:
-            rx_gate = NoisyGate(Rx(-self.s * pi / 2), [self.dataq], self.location + [1])
+            rx_gate = NoisyGate(Rx(-self.s * pi / 2), [self.dataq], self.location + [1], error_model=self.error_model)
             noisy_gates.append(rx_gate)
         return noisy_gates
 
@@ -65,17 +86,17 @@ class ZTypeEntanglingOp(EntanglingOperation):
     """A type of entangling operation which is used by the last four stabiliser steps in the MS-circuit-scheme."""
     def build_noisy_gates(self):
         noisy_gates = []
-        ry_gate = NoisyGate(Ry(self.v*pi/2), [self.dataq], self.location + [0])
+        ry_gate = NoisyGate(Ry(self.v*pi/2), [self.dataq], self.location + [0], error_model=self.error_model)
         noisy_gates.append(ry_gate)
-        rxx_gate = NoisyGate(Rxx(self.s*pi/2), [self.dataq, self.ancillaq], self.location + [1])
+        rxx_gate = NoisyGate(Rxx(self.s*pi/2), [self.dataq, self.ancillaq], self.location + [1], error_model=self.error_model)
         noisy_gates.append(rxx_gate)
         if not self.cancel_data_rx:
-            rx_gate = NoisyGate(Rx(-self.s*pi/2), [self.dataq], self.location + [2])
+            rx_gate = NoisyGate(Rx(-self.s*pi/2), [self.dataq], self.location + [2], error_model=self.error_model)
             noisy_gates.append(rx_gate)
-            ry_gate = NoisyGate(Ry(self.v * pi / 2), [self.dataq], self.location + [3])
+            ry_gate = NoisyGate(Ry(self.v * pi / 2), [self.dataq], self.location + [3], error_model=self.error_model)
             noisy_gates.append(ry_gate)
         else:
-            ry_gate = NoisyGate(Ry(self.v * pi / 2), [self.dataq], self.location + [2])
+            ry_gate = NoisyGate(Ry(self.v * pi / 2), [self.dataq], self.location + [2], error_model=self.error_model)
             noisy_gates.append(ry_gate)
         return noisy_gates
 
@@ -94,7 +115,7 @@ class CZTypeEntangling(EntanglingOperation):
 class StabiliserTimestep():
     """During one StabiliserTimestep, 3 entangling operations are done in parallel."""
 
-    def __init__(self, data=None, ancilla=None, location=None, qu_ind=None, entangling_type="X", cancel_data_rx=None, error_list=None):
+    def __init__(self, data=None, ancilla=None, location=None, qu_ind=None, entangling_type="X", cancel_data_rx=None, error_model=None):
         """
         attr:
             self.location (e.g. [0,0]) [Syndrome measurement location, stabilizer location]
@@ -105,6 +126,7 @@ class StabiliserTimestep():
         self.data = data
         self.ancilla = ancilla
         self.location = location
+        self.error_model=error_model
         self.qu_ind = qu_ind
         self.ent_type = entangling_type
         self.cancel_data_rx = cancel_data_rx
@@ -112,31 +134,31 @@ class StabiliserTimestep():
 
     def build_entangling_operations(self):
         if self.ent_type == "X":
-            ent_op_0 = XTypeEntanglingOp(location=self.location + [0], dataq=self.data[self.qu_ind[0][0]], ancillaq=self.ancilla[self.qu_ind[0][1]], cancel_data_rx=self.cancel_data_rx[0],s=1)
+            ent_op_0 = XTypeEntanglingOp(location=self.location + [0], dataq=self.data[self.qu_ind[0][0]], ancillaq=self.ancilla[self.qu_ind[0][1]], cancel_data_rx=self.cancel_data_rx[0],s=1,error_model=self.error_model)
             ent_op_1 = XTypeEntanglingOp(location=self.location + [1], dataq=self.data[self.qu_ind[1][0]], ancillaq=self.ancilla[self.qu_ind[1][1]],
-                                       cancel_data_rx=self.cancel_data_rx[1], s=1,)
+                                       cancel_data_rx=self.cancel_data_rx[1], s=1, error_model=self.error_model)
             ent_op_2 = XTypeEntanglingOp(location=self.location + [2], dataq=self.data[self.qu_ind[2][0]], ancillaq=self.ancilla[self.qu_ind[2][1]],
-                                       cancel_data_rx=self.cancel_data_rx[2], s=1)
+                                       cancel_data_rx=self.cancel_data_rx[2], s=1, error_model=self.error_model)
         elif self.ent_type == "Z":
             ent_op_0 = ZTypeEntanglingOp(location=self.location + [0], dataq=self.data[self.qu_ind[0][0]], ancillaq=self.ancilla[self.qu_ind[0][1]],
-                                       cancel_data_rx=self.cancel_data_rx[0], s=1, v=1)
+                                       cancel_data_rx=self.cancel_data_rx[0], s=1, v=1, error_model=self.error_model)
             ent_op_1 = ZTypeEntanglingOp(location=self.location + [1], dataq=self.data[self.qu_ind[1][0]], ancillaq=self.ancilla[self.qu_ind[1][1]],
-                                       cancel_data_rx=self.cancel_data_rx[1], s=1, v=1)
+                                       cancel_data_rx=self.cancel_data_rx[1], s=1, v=1, error_model=self.error_model)
             ent_op_2 = ZTypeEntanglingOp(location=self.location + [2], dataq=self.data[self.qu_ind[2][0]], ancillaq=self.ancilla[self.qu_ind[2][1]],
-                                       cancel_data_rx=self.cancel_data_rx[2], s=1, v=1)
+                                       cancel_data_rx=self.cancel_data_rx[2], s=1, v=1, error_model=self.error_model)
         else:
             raise Exception(
                 "Entangling type not recognized, should be \"X\" or \"Z\". Please check stabilizer_timestep instantiation.")
         return [ent_op_0, ent_op_1, ent_op_2]
 
-    def run(self, error_list=None):
+    def run(self, error_list=[]):
         for entangling_operation in self.entangling_operations:
             entangling_operation.run(error_list=error_list)
 
 
 class StabiliserCycle():
     """During one StabiliserCycle, all the data qubits get entangled with all the appropriate ancillas."""
-    def __init__(self, location=None, data=None, ancilla=None, circuit_type="MS"):
+    def __init__(self, location=None, data=None, ancilla=None, circuit_type="MS", error_model=None, error_subset={}):
         '''
         attr:
             location = [0]
@@ -145,8 +167,10 @@ class StabiliserCycle():
         self.ancilla=ancilla
         self.location = location
         self.circuit_type = circuit_type
+        self.error_model = error_model
         self.stabiliser_timesteps = self.build_stabiliser_timesteps(data, ancilla, self.circuit_type)
-        self.gate_locations = self.generate_gate_locations()
+        self.all_gate_locations, self.error_locations, self.gate_locations = self.generate_gate_and_error_locations()
+        self.errors_to_insert = Error.generate_error_list(error_subset, self.error_locations)
 
     def __str__(self):
         string = ""
@@ -154,13 +178,18 @@ class StabiliserCycle():
             string+=str(timestep)
         return string
 
-    def generate_gate_locations(self):
-        gate_locations = []
+    def generate_gate_and_error_locations(self):
+        gate_locations = { Rx : [], Ry : [], Rxx : [] }
+        all_gate_locations = []
+        error_locations = { XCtrlError : [], YCtrlError : [], DephasingError : []}
         for timestep in self.stabiliser_timesteps:
             for entangling_op in timestep.entangling_operations:
                 for noisy_gate in entangling_op.noisy_gates:
-                    gate_locations.append(noisy_gate.location)
-        return gate_locations
+                    all_gate_locations.append(noisy_gate.location)
+                    gate_locations[type(noisy_gate.gate)].append(noisy_gate.location)
+                    for error in noisy_gate.possible_errors:
+                        error_locations[type(error)].append(noisy_gate.location)
+        return all_gate_locations, error_locations, gate_locations
 
     def build_stabiliser_timesteps(self, data, ancilla, circuit_type):
         if len(data) != 9:
@@ -175,7 +204,7 @@ class StabiliserCycle():
         loc_7 = self.location + [7]
 
         # The qubit indices needed for stabiliser_timesteps 1-8
-        # i.e. stabiliser_timestep_1 entangles data[4] & ancilla[1], then data[8] & ancilla[6] etc.
+        # i.e. stabiliser_timestep_0 entangles data[4] & ancilla[1], then data[8] & ancilla[6] etc.
         # The same for all circuit types
         qu_ind_0 = [[4, 1], [8, 6], [6, 4]]
         qu_ind_1 = [[1, 1], [5, 6], [3, 4]]
@@ -198,26 +227,26 @@ class StabiliserCycle():
                               [True, False, False],
                               [False, True, True]]
 
-            stabiliser_timestep_1 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_0, qu_ind=qu_ind_0, entangling_type="X", cancel_data_rx=cancel_data_rx[0])
-            stabiliser_timestep_2 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_1, qu_ind=qu_ind_1, entangling_type="X", cancel_data_rx=cancel_data_rx[1])
-            stabiliser_timestep_3 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_2, qu_ind=qu_ind_2, entangling_type="X", cancel_data_rx=cancel_data_rx[2])
-            stabiliser_timestep_4 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_3, qu_ind=qu_ind_3, entangling_type="X", cancel_data_rx=cancel_data_rx[3])
-            stabiliser_timestep_5 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_4, qu_ind=qu_ind_4, entangling_type="Z", cancel_data_rx=cancel_data_rx[4])
-            stabiliser_timestep_6 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_5, qu_ind=qu_ind_5, entangling_type="Z", cancel_data_rx=cancel_data_rx[5])
-            stabiliser_timestep_7 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_6, qu_ind=qu_ind_6, entangling_type="Z", cancel_data_rx=cancel_data_rx[6])
-            stabiliser_timestep_8 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_7, qu_ind=qu_ind_7, entangling_type="Z", cancel_data_rx=cancel_data_rx[7])
-            stabiliser_timesteps = [stabiliser_timestep_1, stabiliser_timestep_2, stabiliser_timestep_3,
-                                    stabiliser_timestep_4, stabiliser_timestep_5, stabiliser_timestep_6,
-                                    stabiliser_timestep_7, stabiliser_timestep_8]
+            stabiliser_timestep_0 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_0, qu_ind=qu_ind_0, entangling_type="X", cancel_data_rx=cancel_data_rx[0], error_model=self.error_model)
+            stabiliser_timestep_1 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_1, qu_ind=qu_ind_1, entangling_type="X", cancel_data_rx=cancel_data_rx[1], error_model=self.error_model)
+            stabiliser_timestep_2 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_2, qu_ind=qu_ind_2, entangling_type="X", cancel_data_rx=cancel_data_rx[2], error_model=self.error_model)
+            stabiliser_timestep_3 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_3, qu_ind=qu_ind_3, entangling_type="X", cancel_data_rx=cancel_data_rx[3], error_model=self.error_model)
+            stabiliser_timestep_4 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_4, qu_ind=qu_ind_4, entangling_type="Z", cancel_data_rx=cancel_data_rx[4], error_model=self.error_model)
+            stabiliser_timestep_5 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_5, qu_ind=qu_ind_5, entangling_type="Z", cancel_data_rx=cancel_data_rx[5], error_model=self.error_model)
+            stabiliser_timestep_6 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_6, qu_ind=qu_ind_6, entangling_type="Z", cancel_data_rx=cancel_data_rx[6], error_model=self.error_model)
+            stabiliser_timestep_7 = StabiliserTimestep(data=self.data, ancilla=self.ancilla, location=loc_7, qu_ind=qu_ind_7, entangling_type="Z", cancel_data_rx=cancel_data_rx[7], error_model=self.error_model)
+            stabiliser_timesteps = [stabiliser_timestep_0, stabiliser_timestep_1, stabiliser_timestep_2,
+                                    stabiliser_timestep_3, stabiliser_timestep_4, stabiliser_timestep_5,
+                                    stabiliser_timestep_6, stabiliser_timestep_7]
         if circuit_type == "CZ":
             # DO NOTHING
             print("Nothing done.")
         return stabiliser_timesteps
 
-    def run(self, eng, reset=True, error_list=[]):
+    def run(self, eng, reset=True):
 
         for timestep in self.stabiliser_timesteps:
-            timestep.run(error_list=error_list)
+            timestep.run(error_list=self.errors_to_insert)
 
         All(Measure) | self.ancilla
         eng.flush()
@@ -245,7 +274,7 @@ class LogicalQubit():
 
     def measure_syndrome(self, location, error_list):
         stabiliser_cycle = StabiliserCycle(location=location)
-        syndrome = stabiliser_cycle.run(self.data, self.ancilla, self.eng, error_list=error_list)
+        syndrome = stabiliser_cycle.run(self.data, self.ancilla, self.eng)
         return syndrome
 
     def lookup(self, syndrome, display=False):
--- test_logical_qubit.py
+++ test_logical_qubit.py
@@ -1,4 +1,3 @@
-from error import *
 from logical_qubit import *
 from projectq import MainEngine
 from projectq.backends import Simulator
@@ -16,12 +15,29 @@ def test_noisy_gate():
     eng = MainEngine(backend=Simulator(), engine_list=[dummy_engine])
     data = eng.allocate_qureg(1)
     ancilla = eng.allocate_qureg(1)
-    rxx_gate = NoisyGate(Rxx(pi / 2))
-    rxx_gate.apply((data[0], ancilla[0]))
-    rx_gate = NoisyGate(Rx(pi / 2))
-    rx_gate.apply((data[0]))
+    rxx_gate = NoisyGate(Rxx(pi / 2), (data[0], ancilla[0]))
+    rxx_gate.apply()
+    rx_gate = NoisyGate(Rx(pi / 2), (data[0]))
+    rx_gate.apply()
     All(Measure) | data
     All(Measure) | ancilla
     assert len(dummy_engine.received_commands) == 6
 
+def test_stabilizer_cycle():
+    backend = Simulator()
+    dummy_engine = DummyEngine(save_commands=True)
+    eng = MainEngine(backend, [dummy_engine])
+    data = eng.allocate_qureg(9)
+    ancilla = eng.allocate_qureg(8)
+    syndrome0 = StabiliserCycle(data=data, ancilla=ancilla, location=[0], error_model=pdd_error_model)
+    assert len(syndrome0.all_gate_locations) == 60
+    assert len(syndrome0.gate_locations[Rxx]) == 24
+    assert len(syndrome0.gate_locations[Rx]) == 12
+    assert len(syndrome0.gate_locations[Ry]) == 24
+    assert len(syndrome0.error_locations[YCtrlError]) == 24
+    assert len(syndrome0.error_locations[XCtrlError]) == 36
+    assert len(syndrome0.error_locations[DephasingError]) == 60
+
 test_noisy_gate()
+
+test_stabilizer_cycle()
